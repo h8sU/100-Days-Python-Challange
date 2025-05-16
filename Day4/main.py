838c970563d38c271fb81aeabc49d9dc20520dde
h8sU <adomaiciuks.s@gmail.com>
2025-05-16 14:01:36 +0300

    Day4

---
+++ Day4/main.py
@@ -0,0 +1,29 @@
+# Day 4: Amžiaus patikrinimo programa
+# Vartotojas įveda savo gimimo metus, programa paskaičiuoja amžių ir pasako ar vartotojas pilnametis
+
+# Paprasta „Ar galiu eiti į kiną?“ logika
+# Įvedus amžių, programa pasako ar vartotojas gali eiti į kiną (pagal nustatytą amžiaus ribą)
+
+
+from datetime import datetime
+
+#Gauname dabartini laika
+dabartiniai_metai = datetime.now().year
+amziaus_leidimas = 16
+try:
+    # Vartoto ivestis: gimimo metai
+    gimimo_metai = int(input("Iveskite savo gimimo metus: "))
+
+    amzius = dabartiniai_metai - gimimo_metai
+    print(f"Tau dabar yra: {amzius} metai.")
+
+    if gimimo_metai > dabartiniai_metai:
+        print("Klaida! Tu dar nesi gimes.")
+    elif amzius < amziaus_leidimas: 
+        print(f"Tau dar nera {amziaus_leidimas} metu! Deja dar tau negalima")
+    else:
+        print("Valio! Tu gali uzeiti!")
+
+except ValueError:
+    print("Klaida, iveskite tik gimimo metus!")
+
---
+++ Day4/uzduotis.py
@@ -0,0 +1,7 @@
+# Day 4: Amžiaus patikrinimo programa
+# Vartotojas įveda savo gimimo metus, programa paskaičiuoja amžių ir pasako ar vartotojas pilnametis
+
+# Paprasta „Ar galiu eiti į kiną?“ logika
+# Įvedus amžių, programa pasako ar vartotojas gali eiti į kiną (pagal nustatytą amžiaus ribą)
+
+# ---- TAVO KODAS ---- #
